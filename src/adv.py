39603f28ef8eea6af2958b5faceb1bcf7fea8ade
Aruna Benjamin <arunabenji29@gmail.com>
2019-09-07 21:31:33 -0600

    items add/drop

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,25 @@
 from room import Room
 from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",[]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",[]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",[]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",[]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",[]),
 }
 
 
@@ -38,20 +39,26 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+playerInventory = []
+
+roomInventory = []
+
 player = Player('Rocko',room['outside'])
 print(player.room)
 direction=''
 
 while direction!= 'q':
-    direction = input('One: Please enter a direction: ')
+    direction = input('One: Please enter a command: ')
     if(direction== 'n'):
-        if(player.room.n_to!=None):
+        print(player.room.n_to)
+        if(player.room.n_to is not None):
             player.room = player.room.n_to
             print(player.room)
         else:
             print('you cant move in that direction')
     
     elif(direction== 's'):
+        print(player.room.s_to)
         if(player.room.s_to!=None):
             player.room = player.room.s_to
             print(player.room)
@@ -59,6 +66,7 @@ while direction!= 'q':
             print('you cant move in that direction')
 
     elif(direction== 'e'):
+        print(player.room.e_to)
         if(player.room.e_to!=None):
             player.room = player.room.e_to
             print(player.room)
@@ -66,11 +74,69 @@ while direction!= 'q':
             print('you cant move in that direction')
 
     elif(direction== 'w'):
+        print(player.room.w_to)
         if(player.room.w_to!=None):
             player.room = player.room.w_to
             print(player.room)
         else:
             print('you cant move in that direction')
+
+    elif(direction == 'pi'):
+        playerItemName = input('Enter the item name, to your inventory: ')
+
+        playerItemDesc = input('Enter the item decription: ')
+
+        playerInventory.append(Item(playerItemName,playerItemDesc))
+
+        print(f'player Inventory: {playerInventory}')
+
+        player.items = playerInventory    
+        
+        print(player)
+
+    elif(direction == 'ri'):
+        roomItemName = input('Enter the item name, you want to add to a room: ')
+
+        roomItemDesc = input('Enter the item decription, you want to add to a room: ')
+
+        roomInventory.append(Item(roomItemName,roomItemDesc))
+
+        print(f'room Inventory: {roomInventory}')
+
+        player.room.items = roomInventory        
+
+        print(player.room.items)
+
+    elif(len(direction.split(' '))==2):
+        command = direction.split(' ')
+
+        if(command[0].lower() == 'get' or command[0].lower() == 'take'):
+            print(f'the room inventory is: {player.room.items}')
+            print(f'the player inventory is: {player.items}')
+
+            for item in player.room.items:
+                if(item.itemName == command[1]):
+                    player.room.items.remove(item)
+                    print(item.on_take(item.itemName))
+                    player.items.append(item)
+                    print(item.on_drop(item.itemName))
+                else:
+                    print(f'item {command[1]} not found')
+
+        elif(command[0].lower() == 'drop'):
+            print(f'the room inventory is: {player.room.items}')
+            print(f'the player inventory is: {player.items}')
+
+            for item in player.items:
+                if(item.itemName == command[1]):
+                    player.items.remove(item)
+                    player.room.items.append(item)
+                    print(item.on_drop(item.itemName))
+                else:
+                    print(f'item {command[1]} not found')
+
+    elif(direction=='i'):
+        print(f'Current inventory of the player is: {player.items}')
 
 
 # Write a loop that:
@@ -82,4 +148,5 @@ while direction!= 'q':
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
+
 # If the user enters "q", quit the game.
--- src/room.py
+++ src/room.py
@@ -1,18 +1,33 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
+
+# items = [Item('hat','wear on your head'),Item('gloves','wear on your hands'),Item('armour','wear on your chest')]
 
 class Room:
-    def __init__(self,name,description):
+    def __init__(self,name,description,items=[]):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = items
 
     def __repr__(self):
         return f'Room({repr(self.name)}, {repr(self.description)})'
 
     def __str__(self):
-        return f'This is {self.name} has {self.description}'		
+        if self.items:
+            
+            result =  f'This is {self.name} has {self.description}. Items are \n'
+
+            for i in self.items:
+                result += f'     item name: {i.itemName} item description: {i.itemDesc}\n'
+
+            return result
+        else:
+            return f'This is {self.name} has {self.description}'
+
+
 
